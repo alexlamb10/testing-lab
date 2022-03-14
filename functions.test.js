338638f6e82188fcf824c4d170f102768f8a4a20
Alex Lamb <alexlamb104@gmail.com>
2022-03-14 16:26:58 -0600

    Completed challenge, added tests for all math operations

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-const {returnTwo, add, greeting} = require('./function.js')
+const {returnTwo, add, greeting, multiply, divide, subtract} = require('./function.js')
 
 test("returnTwo should equal 2", () => {
     expect(returnTwo()).toBe(2)
@@ -9,7 +9,26 @@ test("greeting should return 'Hello, ' with the input name", () => {
     expect(greeting('Jill')).toBe("Hello, Jill.")
 })
 
-test("Make sure that add finds the sum accurately", () => {
-    expect(add(1, 2)).toBe(3)
-    expect(add(5,9)).toBe(14)
+
+
+describe("Math functions", () => {
+    test("Make sure that add finds the sum accurately", () => {
+        expect(add(1, 2)).toBe(3)
+        expect(add(5,9)).toBe(14)
+    })
+    
+    test("Make sure that multiply finds the product accurately", () => {
+        expect(multiply(1, 2)).toBe(2)
+        expect(multiply(5,9)).toBe(45)
+    })
+    
+    test("Make sure that divide finds the quotient accurately", () => {
+        expect(divide(2, 1)).toBe(2)
+        expect(divide(20, 5)).toBe(4)
+    })
+    
+    test("Make sure that subtract finds the difference accurately", () => {
+        expect(subtract(17, 12)).toBe(5)
+        expect(subtract(88, 33)).toBe(55)
+    })
 })
